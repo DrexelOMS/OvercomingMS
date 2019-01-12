a9e42d10e9e0ea01b5198e0c8adb2fe6b86e4b57
Vincent Finn <vincentfinn@vincents-mac.local>
2019-01-12 14:39:23 -0800

    Added a quick simple example of using alamofire to get food list json

--- OvercomingMS/View Controllers/DietViewController.swift
+++ OvercomingMS/View Controllers/DietViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class DietViewController: UIViewController {
-
+    
+    @IBOutlet weak var foodListTextView: UITextView!
+    
+    private var foodName = ""
+    private var ndbno = ""
+    
+    var foodListUrlPart1 = "https://api.nal.usda.gov/ndb/search/?format=json&q="
+    var foodListUrlPart2 = "&max=25&offset=0&api_key=vhuS0ESO8hNsZ4JB3vpRc1ibIzDqbivrU8SZDCi3";
+    
+    var nutrientsUrlPart1 = "https://api.nal.usda.gov/ndb/V2/reports?ndbno="
+    var nutrientsUrlPart2 = "&type=f&format=json&api_key=vhuS0ESO8hNsZ4JB3vpRc1ibIzDqbivrU8SZDCi3";
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        foodListTextView.text = ""
     }
-
+    
+    @IBAction func TestButton(_ sender: UIButton) {
+        var foodName = "Salmon"
+        Alamofire.request(foodListUrlPart1 + foodName + foodListUrlPart2).responseJSON { (response) in
+            
+            print("Request: \(String(describing: response.request))")
+            print("Response: \(String(describing: response.response))")
+            print("Result: \(String(describing: response.result))")
+            
+            if let json = response.result.value {
+                self.foodListTextView.text = "\(json)"
+            }
+            
+        }
+    }
+    
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
