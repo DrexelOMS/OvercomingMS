6d4823960d4cb1fd098de55443b7a3347f9f21a0
Vince <vince@vinces-imac.local>
2019-01-12 16:44:32 -0800

    Got us nearly back to where we were by using alamofire and swiftyjs to parse nutritional api data and display a list and an alert when clicking an item

--- OvercomingMS/View Controllers/DietViewController.swift
+++ OvercomingMS/View Controllers/DietViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class DietViewController: UIViewController {
+class DietViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var foodListTextView: UITextView!
+    @IBOutlet weak var foodItemTableView: UITableView!
     
-    var foodName = ""
+    var foodItemNames : [String] = []
+    var foodItemIds : [String] = []
+    
+    var searchFoodEntry = ""
     var ndbno = ""
     
     var foodListUrlPart1 = "https://api.nal.usda.gov/ndb/search/?format=json&q="
@@ -28,52 +31,83 @@ class DietViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        foodListTextView.text = ""
+        foodItemTableView.delegate = self
+        foodItemTableView.dataSource = self
     }
+    
+    //MARK: Button Presses
     
     @IBAction func TestButton(_ sender: UIButton) {
         
         let foodName = "Salmon"
         Alamofire.request(foodListUrlPart1 + foodName + foodListUrlPart2).responseJSON { (response) in
             
-            print("Request: \(String(describing: response.request))")
-            print("Response: \(String(describing: response.response))")
-            print("Result: \(String(describing: response.result))")
-            
             if response.result.isSuccess {
                 print("Success! Got the data")
                 
                 let foodJSON : JSON = JSON(response.result.value!)
-                //print(foodJSON.rawString() ?? "")
-                
-                self.foodListTextView.text = ""
-                let foodItems = self.processFoodItems(json: foodJSON)
-                print(foodItems)
-                for name in foodItems {
-                    self.foodListTextView.text += name
-                }
-                
+                self.processFoodItems(json: foodJSON)
+
+                self.foodItemTableView.reloadData()
             }
             else{
                 print("Error: \(String(describing: response.result.error))")
-                self.foodListTextView.text = "Connection Issues"
+                //TODO: add a way to notify the user that a connection could not be established
             }
-            
         }
     }
     
-    func processFoodItems(json : JSON) -> [String] {
-        
-        var stringArray : [String]
-        
-        stringArray = json["list"]["item"].arrayValue.map({$0["name"].stringValue})
-        
-        return stringArray
-        
+    func processFoodItems(json : JSON) {
+        foodItemNames = json["list"]["item"].arrayValue.map({$0["name"].stringValue})
+        foodItemIds = json["list"]["item"].arrayValue.map({$0["ndbno"].stringValue})
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: TableView Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodItemNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = foodItemTableView.dequeueReusableCell(withIdentifier: "FoodItemsCell", for: indexPath) as! FoodItemsCell
+        
+        cell.FoodItemNameLabel.text = foodItemNames[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        Alamofire.request(nutrientsUrlPart1 + foodItemIds[indexPath.row] + nutrientsUrlPart2).responseJSON {
+            (response) in
+            
+            if response.result.isSuccess {
+                print("Success! Got the data")
+                
+                let nutrientJSON : JSON = JSON(response.result.value!)
+                let nutritionalData = nutrientJSON.description
+                //self.processNutritionalInfo(json: nutrientJSON)
+
+                let alertMessage = "You have selected item \(self.foodItemIds[indexPath.row]) with this data \(nutritionalData)"
+                let alertController = UIAlertController(title: "Nutrients", message: alertMessage, preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                alertController.addAction(alertAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+            }
+            else{
+                print("Error: \(String(describing: response.result.error))")
+                //TODO: add a way to notify the user that a connection could not be established
+            }
+        }
+    }
+    
+    func processNutritionalInfo(json: JSON) {
+        //TODO: should open another view and parse the data appropriately
     }
     
 }
