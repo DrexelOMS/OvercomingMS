79310b38403f4e09ec1eff15257b9fe11d0c0fb5
Corey Hensley <cwlhensley@gmail.com>
2019-01-26 13:31:00 -0500

    Issue Resolved

---
+++ OvercomingMS/UI Views/CoreyFrontendUI/ViewControllerExercise.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerExercise.swift
+//  OvercomingMS
+//
+//  Created by Corey Hensley on 1/26/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerExercise: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //TEST TEST FOR VINCE TO SEE
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
