d35c309a110042ecba0f8e12badb9ed28f34885c
Vince <vincent.s.finn@gmail.com>
2019-01-27 10:36:28 -0500

    added roundedCornersUIView class

---
+++ OvercomingMS/UI Views/FinnUITesting/RoundCornersUIView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundCornersUIView.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 1/26/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class RoundCornersUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
