f2ddad5bc2274214c8a3eb424a26265f038b3122
Vince <vincent.s.finn@gmail.com>
2019-01-30 18:08:48 -0500

    added round button class

---
+++ OvercomingMS/UI Views/SharedUIViewSupers/RoundButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundButton.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 1/30/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setup()
+    }
+    
+    private func setup(){
+        layer.cornerRadius = layer.bounds.size.width / 2
+        layer.masksToBounds = true;
+        layoutSubviews()
+    }
+
+}
