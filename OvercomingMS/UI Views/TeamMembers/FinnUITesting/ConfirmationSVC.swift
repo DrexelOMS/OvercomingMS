5519489dfe46bf5e860dbfe0b123200d16ed330f
Vince <vincent.s.finn@gmail.com>
2019-01-30 20:18:29 -0500

    added blue button and confirmation svc s

---
+++ OvercomingMS/UI Views/TeamMembers/FinnUITesting/ConfirmationSVC.swift
@@ -0,0 +1,23 @@
+//
+//  ConfirmationSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 1/30/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationSVC: CustomView {
+    
+    override var nibName: String {
+        get {
+            return "ConfirmationSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+    
+}
