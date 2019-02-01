d3611f087c835087f94a9bad4bf14a6b7dca40bd
Vince <vincent.s.finn@gmail.com>
2019-02-01 15:49:16 -0500

    Added Files ready to implement Quick Food Selection

---
+++ OvercomingMS/UI Views/View Controllers/TrackingVCs/QuickCompleteVCs/QuickCompleteFoodVCViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuickCompleteFoodVCViewController.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 2/1/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class QuickCompleteFoodVCViewController: SwipeDownCloseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
