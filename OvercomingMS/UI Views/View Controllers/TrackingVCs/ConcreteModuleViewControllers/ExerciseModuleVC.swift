ec934453cb060545fff8e88f030af20fdbd4448e
vince <vsf23@drexel.edu>
2019-02-04 15:28:01 -0500

    small refactor to get sliding sac to handle their own data rather than using downcast ting

--- OvercomingMS/UI Views/View Controllers/TrackingVCs/ConcreteModuleViewControllers/ExerciseModuleVC.swift
+++ OvercomingMS/UI Views/View Controllers/TrackingVCs/ConcreteModuleViewControllers/ExerciseModuleVC.swift
@@ -19,21 +19,5 @@ class ExerciseModuleVC: TrackingModuleAbstractVC {
         // Do any additional setup after loading the view.
         initializeStackView(defaultView: ExerciseMainSVC())
     }
-    
-    func addDataItem(){
-        exerciseRoutines.addExerciseItem(routineType: "Test", startTime: Date(), endTime: Date().addingTimeInterval(60*5))
-        updateProgressBarMain(mainPercentage: exerciseRoutines.getTrackingDay()?.ExerciseComputedPercentageComplete ?? 0)
-    }
 
-    func getExerciseItems() -> List<ExerciseRoutinesDBT>? {
-        return exerciseRoutines.getExerciseItems()
-    }
-    
-    func getTotalMinutes() -> Int {
-        return exerciseRoutines.getTrackingDay()?.ExerciseTimeTotal ?? 0
-    }
-    
-    func getPercentageComplete() -> Int {
-        return exerciseRoutines.getTrackingDay()?.ExerciseComputedPercentageComplete ?? 0
-    }
 }
