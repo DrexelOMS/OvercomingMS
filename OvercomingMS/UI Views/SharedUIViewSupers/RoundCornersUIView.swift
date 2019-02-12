a6046aafe4f36d64b547d16d3b49913d6b348801
vince <vsf23@drexel.edu>
2019-02-12 13:46:02 -0500

    updated round corners to be slightly better

--- OvercomingMS/UI Views/SharedUIViewSupers/RoundCornersUIView.swift
+++ OvercomingMS/UI Views/SharedUIViewSupers/RoundCornersUIView.swift
@@ -22,18 +22,50 @@ class RoundCornersUIView: UIView {
     }
     
     private func setup(){
-        layer.cornerRadius = bounds.height / 4
-        layoutSubviews()
-        
-        layer.masksToBounds = false
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
-        layer.shadowRadius = 1.0
-        layer.shadowOpacity = 0.3
-        
-        layer.borderColor = UIColor.gray.cgColor.copy(alpha: 0.5)
-        layer.borderWidth = 1
-        
+        layer.addShadow()
+        layer.roundCorners(radius: bounds.height / 4)
+    }
+
+}
+
+extension CALayer {
+    
+    private func addShadowWithRoundedCorners() {
+        if let contents = self.contents {
+            masksToBounds = false
+            sublayers?.filter{ $0.frame.equalTo(self.bounds) }
+                .forEach{ $0.roundCorners(radius: self.cornerRadius) }
+            self.contents = nil
+            if let sublayer = sublayers?.first,
+                sublayer.name == "subLayer" {
+                
+                sublayer.removeFromSuperlayer()
+            }
+            let contentLayer = CALayer()
+            contentLayer.name = "subLayer"
+            contentLayer.contents = contents
+            contentLayer.frame = bounds
+            contentLayer.cornerRadius = cornerRadius
+            contentLayer.masksToBounds = true
+            insertSublayer(contentLayer, at: 0)
+        }
+    }
+
+    func addShadow() {
+        self.shadowOffset = .zero
+        self.shadowOpacity = 0.2
+        self.shadowRadius = 10
+        self.shadowColor = UIColor.black.cgColor
+        self.masksToBounds = false
+        if cornerRadius != 0 {
+            addShadowWithRoundedCorners()
+        }
+    }
+    func roundCorners(radius: CGFloat) {
+        self.cornerRadius = radius
+        if shadowOpacity != 0 {
+            addShadowWithRoundedCorners()
+        }
     }
 
 }
