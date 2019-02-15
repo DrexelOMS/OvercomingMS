2a691f71ac62bcecaed257115b33f07ca094aaf4
vince <vsf23@drexel.edu>
2019-02-14 20:11:15 -0500

    added code to allow you to add the search bar, and a vertical and height constraint on the FoodSearchSVC

---
+++ OvercomingMS/UI Views/ReusableViews/TrackingModules/FoodModule/SearchBarButton.swift
@@ -0,0 +1,23 @@
+//
+//  SearchBarButton.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 2/14/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarButton: CustomView {
+    
+    override var nibName: String {
+        get {
+            return "SearchBarButton"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+    
+}
