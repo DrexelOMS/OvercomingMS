4f4d874a8c119fe2a8c0536d4087d81d60e35770
vince <vsf23@drexel.edu>
2019-03-03 23:50:13 -0500

    added a way to set custom goals, this is currently global for all days

--- OvercomingMS/Data Models/ProgressBarConfig.swift
+++ OvercomingMS/Data Models/ProgressBarConfig.swift
@@ -11,6 +11,9 @@ import Foundation
 enum TrackingMode { case food, omega3, vitaminD, exercise, meditation, medication }
 
 class ProgressBarConfig {
+    
+    static private let defaults = UserDefaults.standard
+    
     static var foodDescriptions : [String] = ["Aweful.", "Not great.", "Alright.", "Pretty good.", "Perfect!"]
     static func getfoodDescription(rating: Int) -> String {
         if(rating - 1 < 0){
@@ -22,51 +25,67 @@ class ProgressBarConfig {
         return foodDescriptions[rating - 1]
     }
     
+    static private let omega3Key = "omega3Goal"
     static var omega3Goal : Int {
         get {
-            return o3g
+            return getIntFromKey(_default: 100, _key: omega3Key)
         }
         set {
-            o3g = newValue
+            defaults.set(newValue, forKey: omega3Key)
         }
     }
-    static private var o3g = 100
     static let omega3UOM = "g."
     
+    static private let vitaminDKey = "vitaminDGoal"
     static var vitaminDGoal : Int {
         get {
-            return vdg
+            return getIntFromKey(_default: 100, _key: vitaminDKey)
         }
         set {
-            vdg = newValue
+            defaults.set(newValue, forKey: vitaminDKey)
         }
     }
-    static private var vdg = 100
     static let vitaminDUOM = "klUs"
     
+    static private let exerciseKey = "exerciseGoal"
     static var exerciseGoal : Int {
         get {
-            return eg
+            return getIntFromKey(_default: 30, _key: exerciseKey)
         }
         set {
-            eg = newValue
+            defaults.set(newValue, forKey: exerciseKey)
         }
     }
-    static private var eg = 30
     
+    static private let meditationKey = "meditationGoal"
     static var meditationGoal : Int {
         get {
-            return mg
+            return getIntFromKey(_default: 30, _key: meditationKey)
         }
         set {
-            mg = newValue
+            defaults.set(newValue, forKey: meditationKey)
         }
     }
-    static private var mg = 30
     
     static let lengthUOM = "min."
     
     static func calculateKLUs(minutes: Int) -> Int {
         return minutes * 5
+    }
+    
+    static private func getIntFromKey(_default: Int, _key: String) -> Int {
+        if defaults.contains(key: _key){
+            return defaults.integer(forKey: _key)
+        }
+        else {
+            defaults.set(_default, forKey: _key)
+            return defaults.integer(forKey: _key)
+        }
+    }
+}
+
+extension UserDefaults {
+    func contains(key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: key) != nil
     }
 }
