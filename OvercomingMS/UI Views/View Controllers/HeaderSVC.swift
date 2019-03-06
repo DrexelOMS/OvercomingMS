97ecb26ae1756d7cab3e09d2fdf47c6a0a110a48
vince <vsf23@drexel.edu>
2019-03-06 10:59:20 -0500

    extracted the HeaderSVC

---
+++ OvercomingMS/UI Views/View Controllers/HeaderSVC.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 3/6/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class HeaderSVC: CustomView {
+    
+    override var nibName: String {
+        get {
+            return "HeaderSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+    
+}
