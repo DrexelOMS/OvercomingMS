0e4a4efd1fb4b92477d05bdbf7b39de549059152
vince <vsf23@drexel.edu>
2019-03-09 13:07:42 -0500

    change color for the linear progress bars

---
+++ OvercomingMS/UI Views/ReusableViews/TrackingModules/TimerStopwatchSVC/MeditationSetup/MeditationLevelButtonSVC.swift
@@ -0,0 +1,23 @@
+//
+//  MeditationLevelButtonSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 3/9/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class MeditationLevelButtonSVC: CustomView {
+    
+    override var nibName: String {
+        get {
+            return "MeditationLevelButtonSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+    
+}
