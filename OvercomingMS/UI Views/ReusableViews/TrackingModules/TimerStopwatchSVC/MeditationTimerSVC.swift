311887e83fe2f9a4a51725793c566bb11f3c4b9e
Chris <chris@chriss-mac.local>
2019-03-09 17:15:43 -0800

    Timer working

--- OvercomingMS/UI Views/ReusableViews/TrackingModules/TimerStopwatchSVC/MeditationTimerSVC.swift
+++ OvercomingMS/UI Views/ReusableViews/TrackingModules/TimerStopwatchSVC/MeditationTimerSVC.swift
@@ -10,10 +10,52 @@ import UIKit
 
 class MeditationTimerSVC : TimerAbstractSVC {
     
+    private var totalTime = 0
+    private var type = ""
+    
     //allow instantiation with start length in seconds
     convenience init(startingSeconds: Int, meditationType: String) {
         self.init()
         
-        print("Start \(meditationType) timer with \(startingSeconds)")
+        seconds = startingSeconds
+        totalTime = startingSeconds
+        timerLabel.text = timeString(time: Double(seconds))
+        type = meditationType
     }
+    
+    func isDone() -> Bool{
+        if(seconds == 0)
+        {
+            return true
+        }
+        return false
+    }
+    
+    override func changeSeconds() {
+        if(!isDone()){
+            super.changeSeconds()
+        }
+    }
+    
+    override func finishButtonPressed() {
+        super.finishButtonPressed()
+        
+        if(seconds>0)
+        {
+            let minutes = Int(ceil(Double(totalTime - seconds) / 60.0))
+            pushFinishSVC(minutes: minutes)
+        }
+        else
+        {
+            let minutes = Int(ceil(Double(totalTime)/60.0))
+            pushFinishSVC(minutes: minutes)
+        }
+        
+    }
+    
+    
+    override func pushFinishSVC(minutes: Int) {
+        parentVC.pushSubView(newSubView: MeditationAddSVC(startTime: startTime, length: minutes))
+    }
+    
 }
