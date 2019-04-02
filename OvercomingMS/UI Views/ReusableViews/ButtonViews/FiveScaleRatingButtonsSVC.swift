1f65719895e879cb219fdad9095865037b93718f
vince <vsf23@drexel.edu>
2019-04-02 12:01:30 -0400

    Removed team members folder and extracted the ratings from symptoms

---
+++ OvercomingMS/UI Views/ReusableViews/ButtonViews/FiveScaleRatingButtonsSVC.swift
@@ -0,0 +1,21 @@
+//
+//  FiveScaleRatingButtonsSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 4/2/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class FiveScaleRatingButtonsSVC : CustomView {
+    override var nibName: String {
+        get {
+            return "FiveScaleRatingButtonsSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+}
