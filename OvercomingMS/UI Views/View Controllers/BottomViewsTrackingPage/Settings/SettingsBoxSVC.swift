57e7e267efd67cb635e2055fa6fe2a4e0177fed3
Vincent Finn <vincent.s.finn@gmail.com>
2019-04-12 19:26:58 -0400

    added settings views

---
+++ OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Settings/SettingsBoxSVC.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsBoxSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 4/12/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsBoxSVC: CustomView {
+    
+    override var nibName: String {
+        get {
+            return "SettingsBoxSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+}
---
+++ OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Settings/SettingsModifySVC.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsModifySVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 4/12/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsModifySVC : SlidingAbstractSVC {
+    
+    override var nibName: String {
+        get {
+            return "SettingsModifySVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+}
