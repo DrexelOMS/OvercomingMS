e661a07022876cf4b9c33308c7a83de243549d0b
Vincent Finn <vincent.s.finn@gmail.com>
2019-04-12 19:18:16 -0400

    Added Goals Views

---
+++ OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Goals/GoalsBoxSVC.swift
@@ -0,0 +1,22 @@
+//
+//  PillBoxSVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 4/12/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class GoalsBoxSVC : CustomView {
+    
+    override var nibName: String {
+        get {
+            return "GoalsBoxSVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+}
---
+++ OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Goals/GoalsModifySVC.swift
@@ -0,0 +1,23 @@
+//
+//  GoalsModifySVC.swift
+//  OvercomingMS
+//
+//  Created by Vincent Finn on 4/12/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import UIKit
+
+class GoalsModifySVC: SlidingAbstractSVC {
+    
+    override var nibName: String {
+        get {
+            return "GoalsModifySVC"
+        }
+    }
+    
+    override func customSetup() {
+        
+    }
+    
+}
