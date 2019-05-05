d2ce5f5f2dcc98e860289f470a911425d9f258b3
Vincent Finn <vincent.s.finn@gmail.com>
2019-05-05 18:47:52 -0400

    made the font and image expand with the see more button

--- OvercomingMS/UI Views/ReusableViews/ButtonViews/SeeMoreButtonSVC.swift
+++ OvercomingMS/UI Views/ReusableViews/ButtonViews/SeeMoreButtonSVC.swift
@@ -39,10 +39,10 @@ class SeeMoreButtonSVC : CustomView {
     override func layoutSubviews() {
         super.layoutSubviews()
         DispatchQueue.main.async {
-//            let rate = 1 - ((60 - self.frame.height)) / 22
-//            let fontSize = 12  + (6) * rate
-//            print(fontSize)
-//            self.descriptionLabel.font = UIFont(name: self.descriptionLabel!.font.fontName, size: fontSize)
+            let rate = 1 - ((60 - self.frame.height)) / 22
+            let fontSize = 12  + (4) * rate
+            print(fontSize)
+            self.descriptionLabel.font = UIFont(name: self.descriptionLabel!.font.fontName, size: fontSize)
         }
     }
     
