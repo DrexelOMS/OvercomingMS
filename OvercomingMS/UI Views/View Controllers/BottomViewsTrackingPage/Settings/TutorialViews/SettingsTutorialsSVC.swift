8cd36f12cab33865f9613c53d26aa674068d2594
Vincent Finn <vincent.s.finn@gmail.com>
2019-05-15 18:14:34 -0400

    addedTutorialSmall Video

--- OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Settings/TutorialViews/SettingsTutorialsSVC.swift
+++ OvercomingMS/UI Views/View Controllers/BottomViewsTrackingPage/Settings/TutorialViews/SettingsTutorialsSVC.swift
@@ -9,8 +9,46 @@
 import UIKit
 import AVFoundation
 import AVKit
+import Player
 
-class SettingsTutorialsSVC : SlidingAbstractSVC {
+class SettingsTutorialsSVC : SlidingAbstractSVC, PlayerDelegate, PlayerPlaybackDelegate {
+    
+    func playerCurrentTimeDidChange(_ player: Player) {
+        
+    }
+    
+    func playerPlaybackWillStartFromBeginning(_ player: Player) {
+        
+    }
+    
+    func playerPlaybackDidEnd(_ player: Player) {
+        
+    }
+    
+    func playerPlaybackWillLoop(_ player: Player) {
+        
+    }
+    
+    func playerReady(_ player: Player) {
+        
+    }
+    
+    func playerPlaybackStateDidChange(_ player: Player) {
+        
+    }
+    
+    func playerBufferingStateDidChange(_ player: Player) {
+        
+    }
+    
+    func playerBufferTimeDidChange(_ bufferTime: Double) {
+        
+    }
+    
+    func player(_ player: Player, didFailWithError error: Error?) {
+        
+    }
+    
     
     override var nibName: String {
         get {
@@ -28,10 +66,13 @@ class SettingsTutorialsSVC : SlidingAbstractSVC {
     }
     
     @IBOutlet weak var backButton: SquareButtonSVC!
+    @IBOutlet weak var playerView: UIView!
+    
+    var player : Player!
     
     var avPlayer: AVPlayer!
     var playerLayer = AVPlayerLayer()
-    var player = AVPlayer()
+//    var player = AVPlayer()
     
     private var isOnboarding = false
 
@@ -39,24 +80,42 @@ class SettingsTutorialsSVC : SlidingAbstractSVC {
         backButton.backButtonAction = backPressed
     }
     
-    func play(){
-        guard let path = Bundle.main.path(forResource: "Demo_Video", ofType:"mp4") else {
-            debugPrint("video.m4v not found")
-            return
-        }
-        player = AVPlayer(url: URL(fileURLWithPath: path))
-        let playerController = AVPlayerViewController()
-        playerController.player = player
+    override func initialize(parentVC: SlidingStackVC) {
+        super.initialize(parentVC: parentVC)
         
-        playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = self.parentVC.view.bounds
-        parentVC.view.layer.addSublayer(playerLayer)
-        player.play()
+        player = Player()
+        player.playerDelegate = self
+        player.playbackDelegate = self
+        player.view.frame = playerView.bounds
+        
+        parentVC.addChild(player)
+        playerView.addSubview(self.player.view)
+        player.didMove(toParent: parentVC)
+        
+        let videoUrl: URL = URL(fileURLWithPath: Bundle.main.path(forResource: "TutorialSmall", ofType:"mov")!)// file or http url
+        self.player.url = videoUrl
+    }
+    
+    func play(){
+//        guard let path = Bundle.main.path(forResource: "Demo_Video", ofType:"mp4") else {
+//            debugPrint("video.m4v not found")
+//            return
+//        }
+//        player = AVPlayer(url: URL(fileURLWithPath: path))
+//        let playerController = AVPlayerViewController()
+//        playerController.player = player
+//
+//        playerLayer = AVPlayerLayer(player: player)
+//        playerLayer.frame = self.parentVC.view.bounds
+//        parentVC.view.layer.addSublayer(playerLayer)
+//        player.play()
+        
+        self.player.playFromBeginning()
     }
     
     func backPressed() {
-        player.pause()
-        playerLayer.removeFromSuperlayer()
+//        player.pause()
+//        playerLayer.removeFromSuperlayer()
         if isOnboarding {
             parentVC.dismiss()
         }
