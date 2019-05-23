4e1ba7e89136a42d1759fa62896bdd2113d5f807
Justin <jti23@drexel.edu>
2019-05-23 18:18:53 -0400

    starting tests

---
+++ OvercomingMSTests/UI Views/ReusableViews/TrackingModules/FoodModule/FoodSearchSVCTests.swift
@@ -0,0 +1,36 @@
+//
+//  FoodSearchSVCTests.swift
+//  OvercomingMSTests
+//
+//  Created by Justin Ickler on 5/23/19.
+//  Copyright © 2019 DrexelOMS. All rights reserved.
+//
+
+import XCTest
+@testable import OvercomingMS
+
+class FoodSearchSVCTests: XCTestCase {
+
+    var main: FoodSearchSVC!
+
+    override func setUp() {
+        main = FoodSearchSVC()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
